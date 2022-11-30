0f639d58875548471c4da5e11755d770414e148e
charlie lambert <char.lam@ntlworld.com>
2022-11-30 16:36:48 +0000

    ready to be reviewed

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,4 +3,17 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    # The function will need one if branch to check length of string is > 8 then child if branch that will contain multiple || operators to check special chars
+    if(password.length >= 8)
+        if(password.include?("!") || password.include?("@") || password.include?("$") || password.include?("%") || password.include?("&"))
+            true
+        else 
+            false 
+        end 
+    else 
+        false
+    end
+
+    # regex match would be my go to for the special character check but it hasn't been in the course so I left it
+    
 end
