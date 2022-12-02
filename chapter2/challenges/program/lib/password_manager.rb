4ebeff092eccc835a78e3c4d27518f93fb576d65
charlie lambert <char.lam@ntlworld.com>
2022-12-02 14:31:12 +0000

    Practice challenges and Program challenges complete

--- chapter2/challenges/program/lib/password_manager.rb
+++ chapter2/challenges/program/lib/password_manager.rb
@@ -1,2 +1,27 @@
 class PasswordManager
+    def initialize 
+        @store = {}
+    end
+    def add(service, password)
+        if(password.length >= 8 && (password.include?("!") || password.include?("@") || password.include?("$") || password.include?("%") || password.include?("&")))
+            @store[service] = password
+        end
+    end
+    def password_for(service) 
+        @store[service]
+    end
+    def services
+        @store.keys
+    end
 end
+
+my_manager = PasswordManager.new
+my_manager.add("Netflix", "Pass123$")
+my_manager.add("Slack", "Secret123@")
+puts my_manager.password_for("Netflix")
+puts my_manager.services
+
+new_manager = PasswordManager.new
+new_manager.add("Github", "Password123$")
+puts new_manager.services
+
