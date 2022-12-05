cbdc4b41c18ac731de2efe6c42dad9de3925d5fe
charlie lambert <char.lam@ntlworld.com>
2022-12-05 15:41:46 +0000

    Chapter 3 drills complete

--- chapter3/challenges/drills/lib/1_arrays_and_hashes.rb
+++ chapter3/challenges/drills/lib/1_arrays_and_hashes.rb
@@ -7,99 +7,169 @@
 # fourth_element
 # takes one array as an arg
 # returns the fourth element
+def fourth_element(arr)
+    arr[3]
+end
 
 # average
 # takes an array of numbers
 # returns the average (the mean)
+def average(arr)
+    arr.sum/arr.length
+end
 
 # lowest_squared
 # takes an array of numbers
 # returns the lowest number squared
+def lowest_squared(arr)
+    arr.min**2
+end
 
 # highest_squared
 # takes an array of numbers
 # returns the highest number squared
+def highest_squared(arr)
+    arr.max**2
+end
 
 # Hashes I
 
 # values_summed
 # takes a hash where the values are numbers
 # returns the total of all the values
+def values_summed(hash)
+    hash.values.sum
+end
 
 # first_two_keys
 # takes a hash
 # returns the first two keys
+def first_two_keys(hash)
+    hash.keys.slice(0,2)
+end
 
 # first_two_values
 # takes a hash
 # returns the first two values
+def first_two_values(hash)
+    hash.values.slice(0,2)
+end
 
 # Arrays II
 
 # starts_with_a
 # takes one array as an arg
 # returns only elements starting with 'a'
+def starts_with_a(arr)
+    arr.filter{|item| item[0] == "a"}
+end
 
 # starts_with_a_vowel
 # takes an array as an arg
 # returns only the elements that start with a vowel
+def starts_with_a_vowel(arr)
+    arr.filter{|item| "aeiouAEIOU".include?(item[0])}
+end
 
 # reverse_each_element
 # takes an array as an arg
 # reverses each element
 # E.g. when the arg is ['one', 'two']
 # it returns ['eno', 'owt']
+def reverse_each_element(arr)
+    arr.map{|item| item.reverse}
+end
 
 # sort_by_last_letter
 # takes one array as an arg
 # returns the array, sorted by the last letter
+def sort_by_last_letter(arr)
+    arr.sort{|a,b| a[-1] <=> b[-1]}
+end
 
 # greater_than_5
 # takes one array as an arg
 # returns only numbers greater than 5
+def greater_than_5(arr)
+    arr.filter{|item| item > 5}
+end
 
 # greater_than
 # takes one array and one number as args
 # returns only the elements that are greater than the number provided
+def greater_than(arr, num)
+    arr.filter{|item| item > num}
+end
 
 # less_than
 # takes one array and one number as args
 # returns only the elements that are less than the number provided
+def less_than(arr, num)
+    arr.filter{|item| item < num}
+end
 
 # words_shorter_than
 # takes one array and one number as args
 # returns only the elements than have fewer characters than the number provided
+def words_shorter_than(arr, num)
+    arr.filter{|item| item.length < num}
+end
 
 # all_above?
 # takes an array and a number as args
 # returns true if all elements are greater than the number provided
+def all_above?(arr, num)
+    arr.all?{|item| item > num}
+end
 
 # all_below?
 # takes an array and a number as args
 # returns true if all elements are less than the number provided
+def all_below?(arr, num)
+    arr.all?{|item| item < num}
+end
 
 # multiply_each_by
 # takes one array and one number as args
 # returns a new array with each element multiplied by the number provided
+def multiply_each_by(arr, num)
+    arr.map{|item| item * num}
+end
 
 # Hashes II
 
 # where_value_below
 # takes a hash and a number as args
 # returns key value pairs where the value is less than the number provided
+def where_value_below(hash, num)
+    hash.filter{|key, value| value < num}
+end
 
 # where_key_starts_with
 # takes a hash and a letter as args
 # returns key value pairs where the key starts with the letter provided
+def where_key_starts_with(hash, letter)
+    hash.filter{|key, value| key[0] == letter}
+end
 
 # add_key_value_pair
 # takes a hash, a key and a value as args
 # returns a the hash with the new key and value added
+def add_key_value_pair(hash, k, v)
+    hash[k] = v
+    hash
+end
 
 # sort_by_key
 # takes a hash
 # sorts key-value pairs by key
+def sort_by_key(hash)
+    hash.sort_by{|key, value| key}.to_h
+end
 
 # sort_by_value
 # takes a hash
 # sorts key-value pairs by value
+def sort_by_value(hash)
+    hash.sort_by{|key, value| value}.to_h
+end
--- chapter3/challenges/drills/lib/2_classes.rb
+++ chapter3/challenges/drills/lib/2_classes.rb
@@ -44,13 +44,39 @@
 # goodmorning
 # Takes one string as an arg (a name)
 # Returns 'Goodmorning, Alex', if the arg is 'Alex'
-
+class Greeter
+  def hello(name)
+    "Hello, #{name}"
+  end
+  def goodbye(name)
+    "Goodbye, #{name}"
+  end
+  def goodnight(name)
+    "Goodnight, #{name}"
+  end
+  def goodmorning(name)
+    "Goodmorning, #{name}"
+  end
+end
+  
 # Basket
 # add
 # takes one argument of any type
 # adds it to the list of items
 # items
 # returns everything that has been added to the basket
+class Basket
+  def initialize
+    @list = []
+  end
+  def add(arg)
+    @list << arg
+  end
+  def items
+    @list
+  end
+end
+
 
 # Basket2
 # add
@@ -60,6 +86,17 @@
 # returns everything that has been added to the basket
 # eg: after having added "carrots", 3
 # contents returns [{"item" => "carrots", "amount" => 3}]
+class Basket2
+  def initialize
+    @state = []
+  end
+  def add(str, num)
+    @state << {"item" => str, "amount" => num}
+  end
+  def contents
+    @state
+  end
+end
 
 # Calculator
 # add
@@ -82,6 +119,34 @@
 # returns an array containing hashes of the operation, arguments and results of all previous calculations
 # eg: after multiplying 4 and 6, the history is 
 # [{"operation" => "multiply", "arguments" => [4, 6], "result" => 24}]
+class Calculator
+  def initialize
+    @store = []
+  end
+  def add(numOne, numTwo)
+    ans = numOne+numTwo
+    @store << {"operation" => "add", "arguments" => [numOne, numTwo], "result" => ans}
+    ans
+  end
+  def multiply(numOne, numTwo)
+    ans = numOne*numTwo
+    @store << {"operation" => "multiply", "arguments" => [numOne, numTwo], "result" => ans}
+    ans
+  end
+  def subtract(numOne, numTwo)
+    ans = numOne-numTwo
+    @store << {"operation" => "subtract", "arguments" => [numOne, numTwo], "result" => ans}
+    ans
+  end
+  def divide(numOne, numTwo)
+    ans = numOne/numTwo
+    @store << {"operation" => "divide", "arguments" => [numOne, numTwo], "result" => ans}
+    ans
+  end
+  def history
+    @store
+  end
+end
 
 # Cohort
 # add_student
@@ -94,6 +159,20 @@
 # takes one string, the name of an employer, as an arg.
 # E.g. 'NASA'
 # returns only the students who work for that employer
+class Cohort
+  def initialize
+    @state = []
+  end
+  def add_student(hash)
+    @state << hash
+  end
+  def students
+    @state #.map{|item| item["name"]}
+  end
+  def employed_by(str)
+    @state.filter{|item| item["employer"] == str}#.map{|item| item["name"]}
+  end
+end
 
 # Person
 # is initialized with a complex hash, for example...
@@ -128,3 +207,22 @@
 # - a dog called Judith
 # - a goldfish called Gwen
 #
+
+class Person
+  def initialize(hash)
+    @hash = hash
+  end
+  def work_address
+    work = @hash["addresses"].filter{|item| item["name"] == "work"}[0]
+    "#{work["building"]} #{work["street"]}"
+  end
+  def home_address
+    home = @hash["addresses"].filter{|item| item["name"] == "home"}[0]
+    "#{home["building"]} #{home["street"]}"
+  end
+  def pets 
+    sentence = "#{@hash["name"]} has #{@hash["pets"].length} pets\n"
+    @hash["pets"].each{|pet| sentence .concat("- a #{pet["animal"]} called #{pet["name"]}\n")}
+    sentence
+  end
+end
